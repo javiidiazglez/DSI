8dc4570b9bfc278bea3f2d843f715300e840f203
Javi <javiidiazglez@gmail.com>
2022-05-25 02:49:53 +0100

    eliminado p12

--- p12/tests/hello.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {hello} from '../src/hello';
-
-describe('pruebas de ejemplo iniciales', ()=>{
-  it('funcion hello', ()=>{
-    expect(hello()).to.be.eql('hello world');
-  });
-});
